5803b292fdacf281fd29fd955669572c4b6eff3d
SanyaMittal17 <97736276+SanyaMittal17@users.noreply.github.com>
2023-04-08 16:45:43 +0530

    Add files via upload

---
+++ finally_kaam/app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+from flask import Blueprint
+app= Flask(__name__)
+@app.route('/')
+def login():
+    return render_template('login.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+@app.route('/forgotpassword')
+def forgotpassword():
+    return render_template('forgotpassword.html')
+@app.route('/forgotpasswordverify')
+def forgotpasswordverify():
+    return render_template('forgotpasswordverify.html')
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+@app.route('/edit_profile')
+def edit_profile():
+    return render_template('edit_profile.html')
+if __name__ =="__main__":
+    app.run(debug=True)
