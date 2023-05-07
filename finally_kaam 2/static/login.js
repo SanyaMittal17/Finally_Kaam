8bb2e4ac6bd8911c795f2ed08f16fe3986334b7c
Atharva D <97735895+Organic-othello@users.noreply.github.com>
2023-05-07 22:00:41 +0530

    Delete finally_kaam 2 directory

--- finally_kaam 2/static/login.js
+++
@@ -1,6 +0,0 @@
-function handleSubmit () {
-    const email = document.getElementById('email').value;
-    localStorage.setItem("email",email);
-    return;
-    
-}
--- finally_kaam 2/static/profile.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load',() => {
-    const email = localStorage.getItem('email');
-    document.getElementById('email').innerHTML = email;
-    console.log(email);
-})
